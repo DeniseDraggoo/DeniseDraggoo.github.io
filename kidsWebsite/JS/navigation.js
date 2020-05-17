f3638b7e3322d4330e2b272109a80e86a5306a13
Denise Draggoo <34733297+DeniseDraggoo@users.noreply.github.com>
2020-05-17 14:15:29 -0700

    Add files via upload

--- kidsWebsite/JS/navigation.js
+++ kidsWebsite/JS/navigation.js
@@ -2,6 +2,21 @@
 Navigation styles
 ------------------------------------------------------------*/
 
+$("sunIcon").click(function(){
+  $("sunIcon").animate({height: "300px"});
+}); 
+
+
+
+
+/*gsap.timelineLite()
+    .fromto("#sunIcon", 5 {x:300, y:-300}{x:0, y:0});
+
+document.getElementById("#sun-icon").addEventListener("click", playTL);
+
+function playTL() {
+    tl.progress(0).play(); 
+}  */
 
 @media screen and (min-width: 350px) {
 
